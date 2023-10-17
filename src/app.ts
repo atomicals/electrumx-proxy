ce7f50bde44ad756b634e3876fbf5f0ced109661
babcock <bc@atomicalsdev.com>
2023-10-16 21:25:52 -0500

    Add rate limiter

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import * as middlewares from './middlewares';
 import api from './api';
 import MessageResponse from './interfaces/MessageResponse';
 import pretty from 'express-prettify';
+import slowDown  from 'express-slow-down';
 
 export const ServerMessage = { 
   success: true, 
@@ -29,6 +30,19 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json({limit: '1mb', type: 'application/json'}));
 app.use(pretty({ query: 'pretty' }));
+
+if (process.env.TRUST_PROXY === 'true') {
+  app.enable("trust proxy"); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS if you use an ELB, custom Nginx setup, etc)
+}
+
+const speedLimiter = slowDown({
+  windowMs: process.env.RATE_LIMIT_WINDOW_SECONDS ? parseInt(process.env.RATE_LIMIT_WINDOW_SECONDS, 10) * 1000 : 15 * 60 * 1000, // 15 minutes
+  delayAfter: process.env.RATE_LIMIT_DELAY_AFTER ? parseInt(process.env.RATE_LIMIT_DELAY_AFTER, 10) : 100,  // allow 100 requests per 15 minutes, then...
+  delayMs: process.env.RATE_LIMIT_DELAY_MS ? parseInt(process.env.RATE_LIMIT_DELAY_MS, 10) : 500, // begin adding 500ms of delay per request above 100:
+});
+
+//  apply to all requests
+app.use(speedLimiter);
 
 app.get<{}, MessageResponse>('/', (req, res) => {
   res.status(200).json(ServerMessage as any);
