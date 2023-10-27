d4e5d665f0240eeed65f71569200d1d1e739b118
babcock <bc@atomicalsdev.com>
2023-10-26 19:42:19 -0500

    wip

--- src/helpers/decode-urn.ts
+++ src/helpers/decode-urn.ts
@@ -14,10 +14,12 @@ export interface URNInfo {
 
 export const decodeURN = (urn: string): URNInfo => {
   let matched;
-  if (matched = urn.match(/atom\:btc\:dat\:([0-9a-f]{64}i\d+)/)) {
+  // /urn/atom:btc:dat:0d4d83cfb24b6d740031fb937daed09dc77fa4d0a1e46e3b0aa57c992f4863dei0/some/path/image.png
+  if (matched = urn.match(/atom\:btc\:dat\:([0-9a-f]{64}i\d+)(\/.+)?/)) {
     return {
       urnType: URNType.DAT,
       identifier: matched[0],
+      path: matched[1] ? matched[1] : undefined
     };
   }
   console.log('matched', matched)
