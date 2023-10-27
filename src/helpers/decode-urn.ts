a700972de7fbc8e56f1092c6925c8b778737c144
babcock <bc@atomicalsdev.com>
2023-10-26 19:52:04 -0500

    wip

--- src/helpers/decode-urn.ts
+++ src/helpers/decode-urn.ts
@@ -14,12 +14,12 @@ export interface URNInfo {
 
 export const decodeURN = (urn: string): URNInfo => {
   let matched;
-  // /urn/atom:btc:dat:0d4d83cfb24b6d740031fb937daed09dc77fa4d0a1e46e3b0aa57c992f4863dei0/some/path/image.png
   if (matched = urn.match(/atom\:btc\:dat\:([0-9a-f]{64}i\d+)((\/|\$)?.*$)/)) {
     console.log('matched', matched);
     return {
       urnType: URNType.DAT,
       identifier: matched[1],
+      pathType: matched[2],
       path: matched[2] ? matched[2] : undefined,
     };
   }
