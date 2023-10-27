dd00e954dff20b5b8210222c37bd90a7dd2d6cc3
babcock <bc@atomicalsdev.com>
2023-10-26 21:12:29 -0500

    wip

--- src/helpers/urn-response-factory.ts
+++ src/helpers/urn-response-factory.ts
@@ -84,6 +84,7 @@ export class UrnResponseFactory {
     const response = await this.client.general_getRequest('blockchain.transaction.get', [atomicalIdInfo.txid]);
     const fileMap = buildAtomicalsFileMapFromRawTx(response, true);
  
+    console.log('fileMap', fileMap)
     if (fileMap && fileMap['0'] && fileMap['decoded']) {
       res.status(200).json(fileMap['decoded']);
     } else {
