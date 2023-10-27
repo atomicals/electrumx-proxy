50e26dcecfcf7689a5575f70648bff54044a2a38
babcock <bc@atomicalsdev.com>
2023-10-26 19:39:11 -0500

    wip

--- src/api/objectserve.ts
+++ src/api/objectserve.ts
@@ -29,12 +29,12 @@ const handleUrnRequest = async (req, res) => {
   }
 };
 
-router.get<{}, ObjectServeResponse>('/:urn/*', async (req, res) => {
+router.get<{}, ObjectServeResponse>('/:urn', async (req, res) => {
   console.log('urnnnnnn');
   handleUrnRequest(req, res);
 });
 
-router.get<{}, ObjectServeResponse>('/:urn/:path', async (req, res) => {
+router.get<{}, ObjectServeResponse>('/:urn/:path*', async (req, res) => {
   console.log('urnnnnnn path');
   handleUrnRequest(req, res);
 });
