4e1ee2094aaa1ccd871ab492f04e5669edf6a2dd
babcock <bc@atomicalsdev.com>
2023-11-03 17:19:48 -0500

    wip

--- src/helpers/urn-response-factory.ts
+++ src/helpers/urn-response-factory.ts
@@ -35,6 +35,7 @@ export class UrnResponseFactory {
     }
     let path = params['0'];
     let firstimage = false;
+    console.log('req.query', req.query)
     if (!path && req.query.firstimage) {
       firstimage = true;
     }
